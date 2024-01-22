f71d24e3143d0254e02394943541aa7d31cb1464
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2024-01-22 18:58:16 +0700

    fix layout

--- src/components/ListAllProducts.tsx
+++ src/components/ListAllProducts.tsx
@@ -9,11 +9,7 @@ const ListAllProduct = (props: { page: number }) => {
   const { products, loading } = useProductsPagination(props.page);
 
   return (
-    <section
-      className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 ${
-        loading ? "" : "border-t border-black"
-      }`}
-    >
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
       {loading ? (
         <>Loading...</>
       ) : (
