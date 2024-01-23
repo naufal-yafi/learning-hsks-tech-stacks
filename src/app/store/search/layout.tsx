cc345716ecd76e84a226832ffa4862bcb6a8f6e6
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2024-01-23 13:37:55 +0700

    handling loading search

--- src/app/store/search/layout.tsx
+++ src/app/store/search/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+import { FiArrowLeft } from "react-icons/fi";
 
 export const metadata: Metadata = {
   title: "Product Search | Store",
@@ -12,6 +14,14 @@ export default function LayoutSearch({
   return (
     <main>
       <section className="container-padding pt-5">
+        <Link href="/">
+          <button className="mb-5 btn-secondary">
+            <p className="flex-center gap-4">
+              <FiArrowLeft size={"1.5em"} />
+              Home
+            </p>
+          </button>
+        </Link>
         <h1 className="text-xl">Search Product</h1>
         <p className="text-xs mt-1">looking for something you need</p>
       </section>
--- src/app/store/search/page.tsx
+++ src/app/store/search/page.tsx
@@ -3,19 +3,20 @@
 import ListAllProduct from "@component/EachRender/ListAllProducts";
 import useInput from "@hook/useInput";
 import useSearch from "@hook/useSearch";
+import React from "react";
 import { FiX } from "react-icons/fi";
 
 export default function SearchPage() {
   const { inputValue, clear, handleInput, clearInput, handleDeleteKey } =
     useInput();
-  const { products, loading, result, isFind } = useSearch(
+  const { products, loading, result, isFind, loadingSearch } = useSearch(
     inputValue.length < 4 ? "" : inputValue,
   );
 
   return (
-    <>
+    <React.Fragment>
       <section
-        id="search-bar"
+        id="search__input__bar"
         className="container-padding py-4 mt-12 flex-center border-t border-b border-black"
       >
         <input
@@ -38,6 +39,7 @@ export default function SearchPage() {
       </section>
 
       <section
+        id="search__info__require__input"
         className={`container-padding mt-2 text-xs ${
           inputValue.length > 0 && inputValue.length < 4 ? "block" : "hidden"
         }`}
@@ -46,24 +48,34 @@ export default function SearchPage() {
       </section>
 
       <section
+        id="search__response"
         className={`container-padding text-center my-8 ${
           inputValue.length > 3 && !isFind ? "block" : "hidden"
         }`}
       >
-        <h1 className="text-xl">Not Found</h1>
-        <p className="text-xs mt-2">
-          Press{" "}
-          <button
-            className="px-2 mx-1 rounded-sm bg-black text-white"
-            onClick={clearInput}
-          >
-            DELETE
-          </button>{" "}
-          to clear search.
-        </p>
+        {loadingSearch ? (
+          <h1 className="text-xs">Please wait..</h1>
+        ) : (
+          <React.Fragment>
+            <h1 className="text-xl">Not Found</h1>
+            <p className="text-xs mt-2">
+              Press{" "}
+              <button
+                className="px-2 mx-1 rounded-sm bg-black text-white"
+                onClick={clearInput}
+              >
+                DELETE
+              </button>{" "}
+              to clear search.
+            </p>
+          </React.Fragment>
+        )}
       </section>
 
-      <div className={inputValue.length > 3 ? "block" : "hidden"}>
+      <div
+        id="search__result__product"
+        className={inputValue.length > 3 ? "block" : "hidden"}
+      >
         <div
           className={`border-t border-black mt-8 ${
             isFind ? "block" : "hidden"
@@ -80,6 +92,6 @@ export default function SearchPage() {
       >
         <ListAllProduct products={products} loading={loading} />
       </div>
-    </>
+    </React.Fragment>
   );
 }
